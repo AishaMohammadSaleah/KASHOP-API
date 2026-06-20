32a9b9390b5929a373b2d66b92b6c28092ced20b
aisha saleh <aisha.mohd.slaeh@gmail.com>
2026-06-20 23:35:06 +0300

    Add project files.

---
+++ KASHOP.BLL/UsersServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KASHOP.BLL
+{
+    internal class UsersServices
+    {
+        //public List<string> Users() { 
+        
+        //}
+    }
+}
