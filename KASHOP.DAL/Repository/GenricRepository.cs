b7a6021fb8aac466105c4fe022167dd73bf1a4f3
aisha saleh <aisha.mohd.slaeh@gmail.com>
2026-07-13 12:21:44 +0300

    Create Genric Repository

---
+++ KASHOP.DAL/Repository/GenricRepository.cs
@@ -0,0 +1,31 @@
+﻿using KASHOP.DAL.Data;
+using KASHOP.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KASHOP.DAL.Repository
+{
+    public class GenricRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenricRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<T>> GetAllAsync<T>() where T : class
+        {
+            return await _context.Set<T>(). ToListAsync();
+        }
+        public async Task<T> CreateAsync<T>(T entity) where T : class
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
---
+++ KASHOP.DAL/Repository/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KASHOP.DAL.Repository
+{
+    public interface IGenericRepository<T>
+    {
+        Task   <List<T>> GetAllAsync<T>() where T : class;
+        Task<T> CreateAsync<T>(T entity) where T : class;
+    }
+}
